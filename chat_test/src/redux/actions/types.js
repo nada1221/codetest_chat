7fb3f9430dc4c73b2762159542ba2a091eda77a3
nada1221 <nada931221@gmail.com>
2023-01-04 15:24:46 +0900

    feat : redux && firebase etc. change

---
+++ chat_test/src/redux/actions/types.js
@@ -0,0 +1,7 @@
+export const SET_USER = "set_user";
+export const CLEAR_USER = "clear_user";
+export const SET_PHOTOURL = "set_photoURL";
+
+export const SET_CURRENT_CHAT_ROOM = "set_current_chat_room";
+export const SET_PRIVATE_CHAT_ROOM = "set_private_chat_room";
+export const SET_USER_POSTS = "set_user_posts";
